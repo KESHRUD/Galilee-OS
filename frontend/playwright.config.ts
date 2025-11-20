77480c071410d7d6068ed9b0a16aebc28fa62c62
Amine BENHAMMADA <ahmedamine.benhammada@edu.univ-paris13.fr>
2025-11-20 04:06:48 +0100

    debug(pipeline): e2e tests 3

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -42,9 +42,9 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
+    command: "npm run dev",
+    port: 5173,
+    reuseExistingServer: true,
     timeout: 120 * 1000,
-  },
+  }
 });
