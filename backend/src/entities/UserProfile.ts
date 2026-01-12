76f144255cc6ab97073876ee7f749709c4238418
sarah <kafiz.sarah.03@gmail.com>
2026-01-12 11:58:11 +0100

    feat(entities): create UserProfile entity with One-to-One relation to User

--- backend/src/entities/UserProfile.ts
+++ backend/src/entities/UserProfile.ts
@@ -1,4 +1,33 @@
-@OneToOne(() => User, user => user.profile)
-@JoinColumn()
-user: User;
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { User } from "./User";
+
+@Entity("user_profiles")
+export class UserProfile {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column({ default: 0 })
+  xp!: number;
+
+  @Column({ default: 1 })
+  level!: number;
+
+  @OneToOne(() => User, (user) => user.profile, { onDelete: "CASCADE" })
+  @JoinColumn()
+  user!: User;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
 
